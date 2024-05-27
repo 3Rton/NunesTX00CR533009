0e925dcfc364b7755335c204643e857309ef2b53
Ricardo Nunes <3rton93@gmail.com>
2024-05-27 03:29:52 +0300

    Jolt sort of works
    Using with Jolt seems to work now but only with Release and ReleaseWithDebug modes. Debug mode gives runtime static/dynamic lib issues
    (Probably top level cmakelist is not properly managing all other libs to be in same mode but google has nothing to help)

--- Loppuprojekti/main.cpp
+++ Loppuprojekti/main.cpp
@@ -1,12 +1,27 @@
+#include <Jolt/Jolt.h>
+
 #include <iostream>
 
 #include <raylib-cpp.hpp>
 
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
 int main(int, char**){
     std::cout << "Hello, from Loppuprojekti!\n";
 
-    raylib::AudioDevice ad(false);
-    
-    
-    std::cout << "OOP Audio device init: " << std::boolalpha << ad.IsReady();
+    JPH::RegisterDefaultAllocator();
+
+    JPH::Factory::sInstance = new JPH::Factory();
+
+    std::cout << "JPH worksish?";
+
 }
